aed0ad4b8247168613fdb10da070668b735c7285
nguyenbn7 <nguyenbn.email@gmail.com>
2024-03-12 18:37:01 +0700

    add basket mapper

--- server/Application/AutoMapperProfiles.cs
+++ server/Application/AutoMapperProfiles.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Ecommerce.Baskets.Entities;
+using Ecommerce.Baskets.Models;
 using Ecommerce.Products.Entities;
 using Ecommerce.Products.Models;
 
@@ -12,5 +14,14 @@ public class ProductMapper : Profile
             .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
             .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
             .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductImageUrl>());
+    }
+}
+
+public class BasketMapper : Profile
+{
+    public BasketMapper()
+    {
+        CreateMap<CustomerBasket, Basket>();
+        CreateMap<CustomerBasketItem, BasketItem>();
     }
 }
