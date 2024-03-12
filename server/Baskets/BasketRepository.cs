c7aeed872c5f485a804c1257f7338a4eb71aa46b
nguyenbn7 <nguyenbn.email@gmail.com>
2024-03-12 18:34:33 +0700

    BasketRepo implement

---
+++ server/Baskets/BasketRepository.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using Ecommerce.Baskets.Entities;
+using StackExchange.Redis;
+
+namespace Ecommerce.Baskets;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDatabase _db;
+
+    public BasketRepository(IConnectionMultiplexer redis)
+    {
+        _db = redis.GetDatabase();
+    }
+
+    public async Task<bool> DeleteBasketAsync(string basketId)
+    {
+        return await _db.KeyDeleteAsync(basketId);
+    }
+
+    public async Task<Basket?> GetBasketAsync(string basketId)
+    {
+        var data = await _db.StringGetAsync(basketId);
+        if (data.IsNullOrEmpty)
+            return null;
+        return JsonSerializer.Deserialize<Basket>(data.ToString());
+    }
+
+    public async Task<Basket?> UpdateBasketAsync(Basket basket)
+    {
+        var created = await _db.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+
+        if (!created) return null;
+
+        return await GetBasketAsync(basket.Id);
+    }
+}
