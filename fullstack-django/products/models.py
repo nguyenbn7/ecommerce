bcb22a40a1489052c5f9a7238a5347aa45c86f18
nguyenbn7 <nguyenbn.email@gmail.com>
2024-03-13 15:51:21 +0700

    add product tables

--- fullstack-django/products/models.py
+++ fullstack-django/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.TextField(null=False)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=18, decimal_places=2)
+    picture_url = models.TextField(null=False)
+
+
+class ProductBrand(models.Model):
+    name = models.TextField()
+
+
+class ProductType(models.Model):
+    name = models.TextField()
