c41cf3f51bdccfe40b04290d06f6cd02608f1e10
nguyenbn7 <nguyenbn.email@gmail.com>
2024-03-14 16:39:42 +0700

    add docker files and update configuration

--- server/Application/Extension/ForServiceCollection.cs
+++ server/Application/Extension/ForServiceCollection.cs
@@ -47,28 +47,28 @@ public static class ForServiceCollection
         services.Configure<IdentityOptions>(options =>
         {
             // Configure more if needed
-            var passwordSettingsConfig = configuration.GetSection("Identity:Options:Password");
+            var passwordSettingsConfig = configuration.GetSection("IdentityOptions:Password");
 
             // Password settings.
-            options.Password.RequireDigit = !env.IsDevelopment();
-            options.Password.RequireLowercase = !env.IsDevelopment();
-            options.Password.RequireNonAlphanumeric = !env.IsDevelopment();
-            options.Password.RequireUppercase = !env.IsDevelopment();
-            options.Password.RequiredLength = passwordSettingsConfig.GetValue<int>("RequiredLength");
-            options.Password.RequiredUniqueChars = passwordSettingsConfig.GetValue<int>("RequiredUniqueChars");
+            options.Password.RequireDigit = passwordSettingsConfig.GetValue("RequireDigit", options.Password.RequireDigit);
+            options.Password.RequireLowercase = passwordSettingsConfig.GetValue("RequireLowercase", options.Password.RequireLowercase);
+            options.Password.RequireNonAlphanumeric = passwordSettingsConfig.GetValue("RequireNonAlphanumeric", options.Password.RequireNonAlphanumeric);
+            options.Password.RequireUppercase = passwordSettingsConfig.GetValue("RequireUppercase", options.Password.RequireUppercase);
+            options.Password.RequiredLength = passwordSettingsConfig.GetValue("RequiredLength", options.Password.RequiredLength);
+            options.Password.RequiredUniqueChars = passwordSettingsConfig.GetValue("RequiredUniqueChars", options.Password.RequiredUniqueChars);
 
-            var lockoutSettingsConfig = configuration.GetSection("Identity:Options:Lockout");
+            var lockoutSettingsConfig = configuration.GetSection("IdentityOptions:Lockout");
 
             // Lockout settings.
-            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(lockoutSettingsConfig.GetValue<double>("DefaultLockoutTimeSpanMinutes"));
-            options.Lockout.MaxFailedAccessAttempts = lockoutSettingsConfig.GetValue<int>("MaxFailedAccessAttempts");
-            options.Lockout.AllowedForNewUsers = lockoutSettingsConfig.GetValue<bool>("AllowedForNewUsers");
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(lockoutSettingsConfig.GetValue("DefaultLockoutTimeSpanMinutes", options.Lockout.DefaultLockoutTimeSpan.TotalMinutes));
+            options.Lockout.MaxFailedAccessAttempts = lockoutSettingsConfig.GetValue("MaxFailedAccessAttempts", options.Lockout.MaxFailedAccessAttempts);
+            options.Lockout.AllowedForNewUsers = lockoutSettingsConfig.GetValue("AllowedForNewUsers", options.Lockout.AllowedForNewUsers);
 
-            var userSettingsConfig = configuration.GetSection("Identity:Options:User");
+            var userSettingsConfig = configuration.GetSection("IdentityOptions:User");
 
             // User settings.
             options.User.AllowedUserNameCharacters = userSettingsConfig.GetValue<string>("AllowedUserNameCharacters") ?? options.User.AllowedUserNameCharacters;
-            options.User.RequireUniqueEmail = userSettingsConfig.GetValue<bool>("RequireUniqueEmail");
+            options.User.RequireUniqueEmail = userSettingsConfig.GetValue("RequireUniqueEmail", options.User.RequireUniqueEmail);
         });
 
         return services;
